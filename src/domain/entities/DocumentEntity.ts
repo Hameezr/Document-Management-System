eac2eb93a91f4aa4c1c53f14c9afa2b61281bcad
Hameez <hameexr@gmail.com>
2023-08-02 17:12:11 +0500

    updated Document entity

--- src/domain/entities/DocumentEntity.ts
+++ src/domain/entities/DocumentEntity.ts
@@ -1,26 +1,43 @@
 // domain/entities/document/DocumentEntity.ts
-
-import { DocumentId, DocumentTitle } from "../valueObjects/DocumentVO";
+import { DocumentId } from "../valueObjects/DocumentVO";
 
 export class DocumentEntity {
-  private id: DocumentId;
-  private title: DocumentTitle;
-  // Other document properties and methods...
+  private _id: DocumentId;
+  private _title: string;
+  private _content: string;
+  private _createdAt: Date;
+  private _updatedAt: Date;
 
-  constructor(id: DocumentId, title: DocumentTitle) {
-    this.id = id;
-    this.title = title;
-    // Initialize other properties...
+  constructor(id: DocumentId, title: string, content: string) {
+    this._id = id;
+    this._title = title;
+    this._content = content;
+    this._createdAt = new Date();
+    this._updatedAt = new Date();
   }
 
-  // Methods representing document entity behavior...
-
-  // Example methods:
-  public getId(): DocumentId {
-    return this.id;
+  get id(): DocumentId {
+    return this._id;
   }
 
-  public getTitle(): DocumentTitle {
-    return this.title;
+  get title(): string {
+    return this._title;
+  }
+
+  get content(): string {
+    return this._content;
+  }
+
+  get createdAt(): Date {
+    return this._createdAt;
+  }
+
+  get updatedAt(): Date {
+    return this._updatedAt;
+  }
+
+  updateContent(content: string): void {
+    this._content = content;
+    this._updatedAt = new Date();
   }
 }
